1611bbbc6def1c504bb40b7f3a7873c66e5db4ba
Deirdre Storck <deirdre.storck@gmail.com>
2017-11-20 10:18:32 -0800

    new test.sh

--- build/test.sh
+++ build/test.sh
@@ -1,4 +1,15 @@
 #! /bin/sh
 
-test -d "${chart_name}" || exit
+[[ -z "$chart_name"   ]] && \
+  {
+    echo >& "Var '$chart_name' is empty. Cannot continue."
+    exit 2
+  }
+
+[[ ! -d "${chart_name}" ]] && \
+  {
+    echo >&2 "Directory for chart '$chart_name' does not exist."
+    exit 4
+  }
+
 helm lint ${chart_name}
