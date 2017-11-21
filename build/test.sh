c7ed3d7c21b593bec249e0d3c7e5ec07692224e9
Deirdre Storck <deirdre.storck@gmail.com>
2017-11-21 10:14:59 -0800

    fixed error codes

--- build/test.sh
+++ build/test.sh
@@ -2,14 +2,14 @@
 
 [[ -z "$CHART_NAME"   ]] && \
   {
-    echo >& "Var '$CHART_NAME' is empty. Cannot continue."
-    exit 2
+    echo >&2 "Var '$CHART_NAME' is empty. Cannot continue."
+    exit 1
   }
 
 [[ ! -d "${CHART_NAME}" ]] && \
   {
     echo >&2 "Directory for chart '$CHART_NAME' does not exist."
-    exit 4
+    exit 1
   }
 
 helm lint ${CHART_NAME}
